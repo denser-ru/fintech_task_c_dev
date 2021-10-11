9f35cacbe11f867d0eb2331ca93a0b6df8c83cef
Denis <mail@denser.ru>
2021-10-11 08:42:51 +0300

    v.1.2.4 FIX run_test.sh

--- tests/run_test.sh
+++ tests/run_test.sh
@@ -1,15 +1,39 @@
 #!/bin/bash
 
-for (( i = 0; i < 10; i++ ))
+MAX=0
+SUM=0
+n=10
+
+rm ./simulator
+make
+
+
+echo "Измерение версии "$(git branch --show-current)
+
+for (( i = 0; i < n; i++ ))
 do
-  TEST="$({cat data/input.txt | /usr/bin/time -f '%U' ./simulator 1> /dev/null} 2>&1)"
-  if [!$MIN]
+  res1=$(date +%s.%N)
+  cat data/input.txt | ./simulator > /dev/null
+  res2=$(date +%s.%N)
+  TEST=$(echo "$res2 - $res1" | bc)
+  if [[ -z $MIN ]]
   then
     MIN=$TEST
   fi
-  if [$TEST > $MAX]
+  if (( $(echo "$TEST<$MIN" | bc -l) ))
+  then
+    MIN=$TEST
+  fi
+  if (( $(echo "$TEST>$MAX" | bc -l) ))
   then
     MAX=$TEST
   fi
+  SUM=$(bc<<<"$SUM+$TEST")
+  echo -n .
+  sleep 0.1
 done
-echo "min: ${MIN};\t max: ${MAX}"
+echo
+AVR=$(bc<<<"scale=4;$SUM/$i")
+MIN=$(bc<<<"scale=4;$MIN/1")
+MAX=$(bc<<<"scale=4;$MAX/1")
+echo "min: 0${MIN};  max: 0${MAX};  avr: 0${AVR}"
